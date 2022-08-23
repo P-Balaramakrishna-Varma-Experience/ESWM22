ced9f9d9df04736d0f735a9ae5f231e71b1c9515
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-08-23 10:30:03 +0530

    Get state working

--- Backend-App/State.py
+++ Backend-App/State.py
@@ -1,6 +1,7 @@
-import collections
+import bson.json_util as json_util
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+import pymongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://bala:rXs53OGOucrVbPUb@cluster0.je6s2u8.mongodb.net/ESWM22"
@@ -10,13 +11,8 @@ mongo = PyMongo(app)
 @app.get("/")
 def GetState():
     collection = mongo.db.data
-    print(collection)
-    latest = collection.find({})#.sort({'_id':-1})#.limit(1);
-    a =[]
-    for i in latest:
-        a.append(i)
-    print(latest , dir(latest))
-    return jsonify(latest)
+    latest = collection.find().sort("_id", pymongo.DESCENDING).limit(1)
+    return jsonify(json_util.dumps(latest))
 
 
 @app.post("/")
