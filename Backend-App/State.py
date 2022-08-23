7aebf614d7549b5fe759121927c447c99a8148eb
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-08-23 10:40:37 +0530

    Work needed done

--- Backend-App/State.py
+++ Backend-App/State.py
@@ -1,7 +1,8 @@
-import bson.json_util as json_util
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 import pymongo
+import bson.json_util as json_util
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://bala:rXs53OGOucrVbPUb@cluster0.je6s2u8.mongodb.net/ESWM22"
@@ -15,6 +16,11 @@ def GetState():
     return jsonify(json_util.dumps(latest))
 
 
+## No checking for parameters
+### secutyr defence programming
 @app.post("/")
 def SetState():
-    return "<p>Hello, World!</p>"
+    record = request.json
+    collection = mongo.db.data
+    collection.insert_one(record)
+    return "POST request sent"
