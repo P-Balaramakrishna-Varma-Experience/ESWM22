556aeb43ddceea0e491cb7380f0a3925dfd0d606
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2022-09-02 20:16:20 +0530

    PID logic + input changing state done (test pid)

--- Arduino-Code/PID_Code/PID_Code.ino
+++ Arduino-Code/PID_Code/PID_Code.ino
@@ -25,6 +25,19 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
 
 
+double k_p = 2;
+double k_i = 5;
+double k_d = 1;
+bool run = false;
+
+
+
+double rpm_sensor_reading, output, desired = 10;
+unsigned long previousTime;
+double lastError;
+double cumError;
+ 
+
 
 void setup() {
   //intializing serial monitor
@@ -40,6 +53,8 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+
+  desired = 0;                          //set point at zero degreesd
 }
 
 void loop() {
@@ -53,21 +68,29 @@ void loop() {
   
       // JSON.typeof(jsonVar) can be used to get the type of the var
       if (JSON.typeof(parameters) == "undefined") {
-        Serial.println("Parsing input failed!");
+        Serial.println("Parsing rpm_sensor_reading failed!");
         return;
       }
     
       Serial.print("JSON object = ");
       Serial.println(parameters);
+      setGlobalState(parameters);
+      PrintGlobalState();
     }
     else {
       Serial.println("WiFi Disconnected");
     }
     lastTime = millis();
   }
+
+
+  //rpm_sensor_reading = analogRead(A0);                //read from rotary encoder connected to A0
+  //output = computePID(rpm_sensor_reading);
+  //delay(100);
+  //analogWrite(3, output);  
 }
 
-//input the "/" "/del"
+//rpm_sensor_reading the "/" "/del"
 String httpGETRequest(const char* endpoint) {
   WiFiClient client;
   HTTPClient http;
@@ -88,4 +111,45 @@ String httpGETRequest(const char* endpoint) {
 
   http.end();
   return payload;
+}
+
+double computePID(double inp){     
+  double currentTime = millis();                //get current time
+  double elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation
+        
+  double error = desired - inp;                                // determine error
+  cumError += error * elapsedTime;                // compute integral
+  double rateError = (error - lastError)/elapsedTime;   // compute derivative
+ 
+  double out = k_p*error + k_i*cumError + k_d*rateError;                //PID output               
+ 
+  lastError = error;                                //remember current error
+  previousTime = currentTime;                        //remember current time
+ 
+  return out;                                        //have function return the PID output
+}
+
+// messed up json structure
+void setGlobalState(JSONVar state)
+{
+  k_p =  state["data"][0]["k_p"];
+  k_d = state["data"][0]["k_d"];
+  k_i = state["data"][0]["k_i"];
+  run = state["data"][0]["run"];
+  desired = state["data"][0]["desired"];   
+}
+
+void PrintGlobalState()
+{
+  Serial.print(k_p);
+  Serial.print(",");
+  Serial.print(k_d);
+  Serial.print(",");
+  Serial.print(k_i);
+  Serial.print(",");
+  Serial.print(desired);
+  Serial.print(",");
+  Serial.print(run);
+  Serial.print(",");  
+  Serial.println(" ");
 }
